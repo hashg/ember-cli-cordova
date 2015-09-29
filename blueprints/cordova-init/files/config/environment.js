62a7298cbb9c0ae3033c08c075ec07db4cf2fc51
harish.gowda <harish.gowda@gmail.com>
2015-09-28 22:22:28 -0700

    removed bind-attr

--- blueprints/cordova-init/files/config/environment.js
+++ blueprints/cordova-init/files/config/environment.js
@@ -21,6 +21,16 @@ module.exports = function(environment) {
     cordova: {
       rebuildOnChange: false,
       emulate: false
+    },
+
+    contentSecurityPolicy : {
+      'default-src': "'none'",
+      'script-src': "'self' ", 
+      'font-src': "'self' ", 
+      'connect-src': "'self' ", 
+      'img-src': "'self'",
+      'style-src': "'self' 'unsafe-inline' ", 
+      'media-src': "'self'"
     }
   };
 
